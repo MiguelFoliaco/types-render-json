fe6fed407dbcd6c5bc548d14600dfb9dd37db827
Miguel Foliaco <foliaco18@outlook.es>
2023-04-16 14:38:37 -0500

    [feat] add iterface code

--- src/paragraph.ts
+++ src/paragraph.ts
@@ -1,12 +1,12 @@
 import { CSSProperties } from "react";
 
 export interface IParagraph {
-    name?: string;
-    target: "p";
-    level?: number;
-    typetext?: "text";
-    style?: CSSProperties;
-    text: string;
-    __type?: "Paragraph";
-  }
-  
+  name?: string;
+  target: "p";
+  level?: number;
+  typetext?: "text";
+  style?: CSSProperties;
+  text: string;
+  markdown?: boolean;
+  __type?: "Paragraph";
+}
