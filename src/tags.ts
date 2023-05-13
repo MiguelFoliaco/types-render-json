a0b2d51d32185d37333a95d118f03acedc90820b
Miguel Foliaco <foliaco18@outlook.es>
2023-05-13 00:38:06 -0500

    [Update] add ITags

---
+++ src/tags.ts
@@ -0,0 +1,23 @@
+import { IMultiTargetTitle } from "./title";
+
+export type ITags =
+    | "p"
+    | "Main"
+    | IMultiTargetTitle
+    | "flex"
+    | "link"
+    | "form"
+    | "img"
+    | "categories"
+    | "product-cards"
+    | "product-name"
+    | "product-image"
+    | "product-description"
+    | "input"
+    | "code"
+    | "page"
+    | "button-social"
+    | "top-bar"
+    | "icon"
+    | "list"
+    | "Custom";
